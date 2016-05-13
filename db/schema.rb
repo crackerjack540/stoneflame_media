03ef71bbd0a66cd1ea91e345c2fbe5a4c21c513e
crackerjack540 <awheat1989@gmail.com>
2016-05-12 20:07:03 -0400

    reupload

--- db/schema.rb
+++ db/schema.rb
@@ -20,11 +20,4 @@ ActiveRecord::Schema.define(version: 20160229040753) do
     t.datetime "updated_at", null: false
   end
 
-  
-  create_table "messages", force: :cascade do |t|
-    t.string   "title"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
 end
