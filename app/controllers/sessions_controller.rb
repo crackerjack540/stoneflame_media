93766d179e7db93fbcebf1e4dfe0e1f38cca87b4
crackerjack540 <awheat1989@gmail.com>
2016-05-30 16:15:44 -0400

    updated login to use username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,11 @@
 class SessionsController < ApplicationController
+ 
   def new
   end
   
   def create
-  	user = User.find_by_email(params[:email])
-  	if user && user.authenticate(params[:email],[:password])
+  	user = User.find_by_username(params['/login'][:username])
+  	if user && user.authenticate(params[:password])
   	session[:user_id] = user.id
   	redirect_to root_path, notice: "You are now logged in."
   else 
